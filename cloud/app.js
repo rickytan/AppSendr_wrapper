196d3b5a8e5fd95db0fae7110b20ca5b8c4dd731
rickytan <ricky.tan.xin@gmail.com>
2014-03-04 13:56:03 +0800

    add

--- cloud/app.js
+++ cloud/app.js
@@ -12,5 +12,9 @@ app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
+app.get('/:id', function(req, res) {
+	res.render('install', { app: {name: "DreamColor"}});
+});
+
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
